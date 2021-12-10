3c4fc6a92d4d3d1adf506c98af7c8f598ed0f28b
Aladiel <jonathan.mougin0@gmail.com>
2021-12-10 09:39:14 +0100

    mini modif

--- src/main/java/fr/eni/projetEncheres/servlets/ServletConnection.java
+++ src/main/java/fr/eni/projetEncheres/servlets/ServletConnection.java
@@ -1,7 +1,6 @@
 package fr.eni.projetEncheres.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
