ff397c9bb4eca05a282e9fefdfca9376766d417b
duchenedaphne <duchenedaphne@outlook.com>
2021-12-10 12:22:02 +0100

    ajout des objets Utilisateur et Categorie en attributs

--- src/main/java/fr/eni/projetEncheres/bo/Enchere.java
+++ src/main/java/fr/eni/projetEncheres/bo/Enchere.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 public class Enchere {
 	// Attributs :
 	private int no_enchere; //@Julian
-	private int no_utilisateur;
+	Utilisateur utilisateur;
     private int no_article;
     private LocalDate date_enchere;
     private int montant_enchere;
@@ -17,9 +17,9 @@ public class Enchere {
     public Enchere() {
     }
     
-	public Enchere(int no_utilisateur, int no_article, LocalDate date_enchere, int montant_enchere) {
+	public Enchere(Utilisateur utilisateur, int no_article, LocalDate date_enchere, int montant_enchere) {
 		super();
-		this.no_utilisateur = no_utilisateur;
+		this.utilisateur = utilisateur;
 		this.no_article = no_article;
 		this.date_enchere = date_enchere;
 		this.montant_enchere = montant_enchere;
@@ -35,12 +35,12 @@ public class Enchere {
 		this.no_enchere = no_enchere;
 	}
 	
-	public int getNo_utilisateur() {
-		return no_utilisateur;
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
 	}
 
-	public void setNo_utilisateur(int no_utilisateur) {
-		this.no_utilisateur = no_utilisateur;
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
 	}
 
 	public int getNo_article() {
@@ -70,7 +70,7 @@ public class Enchere {
     // Overrive toString :
 	@Override
 	public String toString() {
-		return "Enchere [no_utilisateur=" + no_utilisateur + ", no_article=" + no_article + ", date_enchere="
+		return "Enchere [utilisateur=" + utilisateur + ", no_article=" + no_article + ", date_enchere="
 				+ date_enchere + ", montant_enchere=" + montant_enchere + "]";
 	}
    
