a37b65a71557329f88376b4543e25561426c565f
GukanLJ <julian.senechal@gmail.com>
2021-12-14 10:17:02 +0100

    DAO = Modfication FactoryDAO, utilisateurDAO et suppression jdbctools

--- src/main/java/fr/eni/projetEncheres/dal/dao/UtilisateurDAO.java
+++ src/main/java/fr/eni/projetEncheres/dal/dao/UtilisateurDAO.java
@@ -1,10 +1,32 @@
 package fr.eni.projetEncheres.dal.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import fr.eni.projetEncheres.BusinessException;
+import fr.eni.projetEncheres.bo.Utilisateur;
+
 /**
  * @author Daphné
  */
 public interface UtilisateurDAO {
-
+	
+	//insert update delete
+	
+	public void insert(Utilisateur u)throws  SQLException, BusinessException;
+	
+	public void update(Utilisateur u)throws  SQLException, BusinessException;
+	
+	public void delete(int no_utilisateur)throws  SQLException, BusinessException;
+	
+	//select
+	
+	public Utilisateur selectUtilisateurById(int no_utilisateur)throws  SQLException, BusinessException;
+	
+	public List<Utilisateur> select()throws  SQLException, BusinessException;
+	
+	public List<Utilisateur> selectAllUtilisateur()throws  SQLException, BusinessException;
+	
 	
 }
 
