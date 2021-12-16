5bacada302087f370bd8ce7cb869fc6c075bd198
duchenedaphne <duchenedaphne@outlook.com>
2021-12-16 20:42:58 +0100

    fonctionnalité "Supprimer mon compte"

--- src/main/java/fr/eni/projetEncheres/servlets/ServletNouvelleVente.java
+++ src/main/java/fr/eni/projetEncheres/servlets/ServletNouvelleVente.java
@@ -8,7 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+/**
+ * @author Daphné
+ */
 /**
  * Servlet implementation class ServletNouvelleVente
  */
